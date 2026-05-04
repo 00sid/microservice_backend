da145e6779e5d592ad67f256a3039fd0e65dbe31
00sid <siddbasnet2002@gmail.com>
2026-05-04 15:03:36 +0545

    Media-Service: Media Routes created

---
+++ media-service/src/routes/media-routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const multer = require("multer");
+const authenticateRequest = require("../middleware/authMiddleware");
+const logger = require("../utils/logger");
+const { uploadMedia } = require("../controllers/media-controller");
+
+const router = express.Router();
+
+// configure multer for file upload
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+}).single("file");
+
+router.post(
+  "/upload",
+  authenticateRequest,
+  (req, res, next) => {
+    upload(req, res, function (err) {
+      if (err instanceof multer.multer) {
+        logger.error("Multer error while uploading", err);
+        res.status(400).json({
+          success: false,
+          message: "Multer error while uploading",
+          error: err.message,
+          stack: err.stack,
+        });
+      } else if (err) {
+        logger.error("Unknown error while uploading", err);
+        res.status(500).json({
+          success: false,
+          message: "Unknown error while uploading",
+          error: err.message,
+          stack: err.stack,
+        });
+      }
+      if (!req.file) {
+        res.status(400).json({
+          success: false,
+          message: "No file found!",
+        });
+      }
+      next();
+    });
+  },
+  uploadMedia,
+);
+
+module.exports = router;
